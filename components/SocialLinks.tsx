f8744311ad6ab8444674bbd489d2daacde06ec4c
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2026-09-12 19:03:29 +0530

    Add Calendly meeting link

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { FaGithub, FaXTwitter } from "react-icons/fa6";
-import { FileText, Mail, Check } from "lucide-react";
+import { CalendarDays, FileText, Mail, Check } from "lucide-react";
 
 const iconLinks = [
   {
@@ -19,6 +19,11 @@ const iconLinks = [
     label: "Resume",
     href: "/resume.pdf",
     Icon: FileText,
+  },
+  {
+    label: "Book a meeting",
+    href: "https://calendly.com/ashuswain9876/30min",
+    Icon: CalendarDays,
   },
 ];
 
